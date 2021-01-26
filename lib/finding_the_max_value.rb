b887e8b1b8eac190cffd6cc8d080290d0d64a8fd
Phong Lo <phonglo724@gmail.com>
2021-01-26 00:27:03 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def find_max_value(array)
-  array.max()
+  count = 0
+  max_value = -1
+  while count < array.length do
+    if max_value < array[count]
+      max_value = array[count]
+      binding.pry
+    end
+    count += 1
+  end
+  max_value
 end
+
+
+
+#def find_max_value#(array)
+  #array.max()
+#end
